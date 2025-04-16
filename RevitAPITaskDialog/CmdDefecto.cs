1e3ded3c5b171474146ecd9c3a9872a1416f31d5
LuisAlonsoDev <arqluisquispegalarza.bim@gmail.com>
2025-04-15 20:44:02 -0500

    Agregar archivos de proyecto.

---
+++ RevitAPITaskDialog/CmdDefecto.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITaskDialog
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class CmdDefecto : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog mywindow = new TaskDialog("mi Ventana");
+            mywindow.Title = "Hola Cronos";
+            mywindow.TitleAutoPrefix = false;
+            mywindow.MainIcon = TaskDialogIcon.TaskDialogIconInformation;
+            mywindow.MainInstruction = "Saludos...";
+            mywindow.MainContent = "Soy el arquitecto Luis Quispe Galarza" +
+                                   " apasionado por el desarrollo de nuevas" +
+                                   " tecnologias en la metodologia BIM, me dedico a trabajar en proyectos de" +
+                                   " distintas caracteristicas como hospitalarios, educativos, subestacion y entre" +
+                                   " otros. Tambien me desempeño en desarrollar aplicaciones tipo Addin en el" +
+                                   " entorno de desarrollo de Revit.";
+            mywindow.FooterText = "Copyright (C) 2023 by Cronos Arquitectura e Ingenieria";
+            mywindow.ExpandedContent = "Si necesita saber mas sobre el aplicativo puede revisar los videos " +
+                                        "dentro de nuestro canal de Youtube CronosBim o comunicarse al numero de celular 988706396";
+            mywindow.VerificationText = "En Cronos transformamos proyectos con innovación";
+            mywindow.CommonButtons = TaskDialogCommonButtons.Cancel |
+                                    TaskDialogCommonButtons.Retry |
+                                    TaskDialogCommonButtons.None;
+            mywindow.Show();
+            return Result.Succeeded;
+        }
+    }
+}
